c8dd45210b1ad4c3254a2ea2950dd1d048da8c5b
shari-sushi <susi.imo.niku@gmail.com>
2024-12-27 01:52:26 +0900

    wip

--- t0016Next/myapp/src/components/box/Box.tsx
+++ t0016Next/myapp/src/components/box/Box.tsx
@@ -24,3 +24,7 @@ import { FormTW } from "@/styles/tailwiind";
 export const NeedBox = () => {
   return <span className={`${FormTW.need} mx-1`}>必須</span>;
 };
+
+export const DisableBox = () => {
+  return <span className={`bg-[#36a91c] ${FormTW.auto} mx-1`}>自動可</span>;
+};
--- t0016Next/myapp/src/components/form/Common.tsx
+++ t0016Next/myapp/src/components/form/Common.tsx
@@ -47,3 +47,91 @@ export const SelectCrudContent = ({
     </div>
   );
 };
+
+type getYoutubeVideoProps = {
+  movieId: string;
+  isSnippet?: boolean;
+  isContentDetails?: boolean;
+  isStatus?: boolean;
+  isStatistics?: boolean;
+  isPlayer?: boolean;
+  isTopicDetails?: boolean;
+  isRecordingDetails?: boolean;
+  isFileDetails?: boolean;
+  isProcessingDetails?: boolean;
+  isSuggestions?: boolean;
+  isLiveStreamingDetails?: boolean;
+  isLocalizations?: boolean;
+};
+
+export const getYoutubeMovieTitle = async ({
+  movieId,
+}: getYoutubeVideoProps): Promise<any> => {
+  // `&key=${process.env.YOUTUBE_API_KEY}`
+  const url =
+    `https://www.googleapis.com/youtube/v3/` +
+    `videos?id=${movieId}` +
+    `&key=${"AIzaSyBPH1X5ShgFOIzGWijZqrgnegkC6lT6_Fw"}` +
+    `&part=snippet`;
+
+  try {
+    const res = await fetch(url);
+    if (!res.ok) {
+      throw new Error("error in getYoutubeMovie");
+    }
+    // @ts-ignore
+    return await res.json().items[0].snippet.title;
+  } catch (error) {
+    console.log("error in getYoutubeMovie:", error);
+    return null;
+  }
+};
+
+export const getYoutubeMovie = async ({
+  movieId,
+  isSnippet,
+  isStatistics,
+  isContentDetails,
+  isStatus,
+  isPlayer,
+  isTopicDetails,
+  isRecordingDetails,
+  isFileDetails,
+  isProcessingDetails,
+  isSuggestions,
+  isLiveStreamingDetails,
+  isLocalizations,
+}: getYoutubeVideoProps): Promise<any> => {
+  const url = `https://www.googleapis.com/youtube/v3/videos?id=${movieId}&key=${"AIzaSyBPH1X5ShgFOIzGWijZqrgnegkC6lT6_Fw"}${
+    isSnippet && "&part=snippet"
+  }`;
+
+  try {
+    const res2 = await fetch(
+      // `&key=${process.env.YOUTUBE_API_KEY}`
+      `https://www.googleapis.com/youtube/v3/` +
+        `videos?id=${movieId}` +
+        `&key=${"AIzaSyBPH1X5ShgFOIzGWijZqrgnegkC6lT6_Fw"}` +
+        `${isSnippet && "&part=snippet"}` +
+        `${isContentDetails && "&part=contentDetails"}` +
+        `${isStatus && "&part=statistics"}` +
+        `${isStatistics && "&part=statistics"}` +
+        `${isPlayer && "&part=player"}` +
+        `${isTopicDetails && "&part=topicDetails"}` +
+        `${isRecordingDetails && "&part=recordingDetails"}` +
+        `${isFileDetails && "&part=fileDetails"}` +
+        `${isProcessingDetails && "&part=processingDetails"}` +
+        `${isSuggestions && "&part=suggestions"}` +
+        `${isLiveStreamingDetails && "&part=liveStreamingDetails"}` +
+        `${isLocalizations && "&part=localizations"}`
+    );
+    const res = await fetch(url);
+    if (!res.ok) {
+      throw new Error("error in getYoutubeMovie");
+    }
+    return await res.json();
+  } catch (error) {
+    console.log("error in getYoutubeMovie:", error);
+    return null;
+  }
+};
